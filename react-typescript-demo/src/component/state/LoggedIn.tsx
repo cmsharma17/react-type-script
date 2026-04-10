6af8ea58beb9b5dba9d970f05e6f0a3acfb993c3
CHANDRA MOULI SHARMA <cmsharma.cs@gmail.com>
2026-04-10 06:54:25 +0200

    additional type script example

---
+++ react-typescript-demo/src/component/state/LoggedIn.tsx
@@ -0,0 +1,13 @@
+
+
+export function LoggedIn(){
+
+    const handleLogin = () => {}
+    const handleLogout = () => {}
+
+    return(<div>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>Logout</button>
+        <div>User is LoggedIn/LoggedOut</div>
+    </div>)
+}
